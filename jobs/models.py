3fcb6474a77643418a489745ac741af68a205b5c
syamsruthin <syamsruthin.k@gmail.com>
2020-03-22 01:23:06 +0100

    adding Jobs app

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=500)
