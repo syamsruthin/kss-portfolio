f4c4acabb011a517612bd0f455a56368d61aa2f5
syamsruthinkaliki <syamsruthin.kaliki@gmail.com>
2020-12-12 19:04:21 +0100

    adding hosts in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ps5x%a&h@76--4ydi!_9^2@f0k1h58i_69*7@a0lhx)a635)36'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['syamsruthinkaliki.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
