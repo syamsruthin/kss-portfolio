c81b4077250e9d9a3c592c69970cd6900349eb2b
syamsruthinkaliki <syamsruthin.kaliki@gmail.com>
2021-01-04 12:08:23 +0100

    test line breaks

--- jobs/models.py
+++ jobs/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=100)
-    summary = models.CharField(max_length=500)
+    summary = models.TextField(max_length=1000)
